5d3ac170c3397b4677d51c5b76f070c9426df422
Бобров Олег <yourdeath651@gmail.com>
2025-08-18 16:51:18 +0200

    Добавил логику аватара и fallback на иконку

--- scripts.js
+++ scripts.js
@@ -1,4 +1,17 @@
 let tg = window.Telegram.WebApp;
 
-document.getElementById("user_name_show").innerHTML = tg.initData.user.first_name + " " + tg.initData.user.last_name;
-console.log(tg.initData.photo_url);
+
+document.getElementById("user_name_show").innerText =
+    tg.initDataUnsafe.user.first_name + " " + (tg.initDataUnsafe.user.last_name ?? "");
+
+
+let avatarContainer = document.getElementById("user_avatar");
+
+if (tg.initDataUnsafe.user.photo_url) 
+{
+    avatarContainer.innerHTML = `<img src="${tg.initDataUnsafe.user.photo_url}" style="border-radius: 5px;" alt="user photo">`;
+} 
+else 
+{
+    avatarContainer.innerHTML = `<i class="fa-solid fa-circle-user fa-2x"></i>`;
+}
